29b42e8ba5fcb7876401577f0aeed07c9856b67c
ivalchanka <i.valchenk@hotmail.com>
2021-01-27 23:30:19 +0300

    Added factory method example

---
+++ FactoryMethod/Common/Sql/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace FactoryMethod.Common.Sql
+{
+    public abstract class Helper
+    {
+        public string Query { get; set; }
+
+        public abstract Helper CreateHelper(int partnerId);
+
+        public abstract T ExecuteAsScalar<T>() where T : struct;
+
+        public abstract DataTable ExecuteAsDataTable();
+
+        public abstract IDataReader ExecuteAsDataReader();
+    }
+}
---
+++ FactoryMethod/Common/Sql/SecurityHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace FactoryMethod.Common.Sql
+{
+    public class SecurityHelper : Helper
+    {
+        public override Helper CreateHelper(int partnerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IDataReader ExecuteAsDataReader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable ExecuteAsDataTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T ExecuteAsScalar<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
