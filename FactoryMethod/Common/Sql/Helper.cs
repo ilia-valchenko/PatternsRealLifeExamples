80b8518f99c291fbb57af8f3a05bda1845e3f874
ivalchanka <i.valchenk@hotmail.com>
2021-02-02 23:24:30 +0300

    Added one more comment.

--- FactoryMethod/Common/Sql/Helper.cs
+++ FactoryMethod/Common/Sql/Helper.cs
@@ -9,6 +9,8 @@ namespace FactoryMethod.Common.Sql
         // The factory method.
         // It allow us to work with different types of objects by using a single interface.
         // Each new concrete creator brings you closer and closer to the abstract factory.
+
+        // We can think about a factory method as a strategy of creating objects.
         public abstract IDbConnection CreateDatabaseConnection();
 
         public IDataReader ExecuteAsDataReader()
