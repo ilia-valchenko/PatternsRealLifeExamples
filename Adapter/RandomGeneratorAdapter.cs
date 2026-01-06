b66edc1d9546f3b4aca06fa58a6b04369ebcfe26
Ilya Valchanka <i.valchenk@hotmail.com>
2026-01-06 11:27:01 +0300

    Added a few comments.

--- Adapter/RandomGeneratorAdapter.cs
+++ Adapter/RandomGeneratorAdapter.cs
@@ -1,5 +1,8 @@
 ﻿namespace Adapter
 {
+    // Note: Wraps incompatible object.
+    // Allows the interface of an existing class to be used as another interface.
+
     // The Adapter pattern is based on composition.
     // It wraps only one class.
     public class RandomGeneratorAdapter : RandomGenerator, IGenerator
